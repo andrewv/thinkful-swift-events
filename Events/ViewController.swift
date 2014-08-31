8d754e8bf53869660a078944809d7fe299c81b25
Andrew Varvel <andy@fastmail.com.au>
2014-08-31 21:37:43 +1000

    Initial commit

--- Events/ViewController.swift
+++ Events/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
                             
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var subtitleTextField: UITextField!
+    
+    @IBAction func addEvent(sender: AnyObject) {
+        let dataVC = self.childViewControllers.last as OutputViewController
+        dataVC.addNewEvent((titleTextField.text), subtitle: (subtitleTextField.text))
+        resetUI()
+        
+    }
+    
+    func resetUI() {
+        titleTextField.text = ""
+        subtitleTextField.text = ""
+        self.view.endEditing(true)
+        }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -23,3 +41,29 @@ class ViewController: UIViewController {
 
 }
 
+
+class OutputViewController:UIViewController {
+    
+    @IBOutlet weak var outputTextView: UITextView!
+    
+    func addNewEvent(title:String, subtitle:String) {
+        var newTitle = title
+        var newSubtitle = subtitle
+        outputTextView.text = outputTextView.text.stringByAppendingFormat("\n%@", newTitle)
+        outputTextView.text = outputTextView.text.stringByAppendingFormat("\n%@", newSubtitle)
+        outputTextView.text = outputTextView.text.stringByAppendingFormat("\n%@", "")
+
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
